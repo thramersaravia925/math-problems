15d7228b00b53766f8fdaa6d0f294a73fad436eb
thramersaravia925 <thramersaravia925@gmail.com>
2025-03-06 07:25:52 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,11 @@
+
+int main() {
+    int x = 5;
+    int y = 10;
+    if (x > y) {
+        printf("x is greater than y");
+    } else {
+        printf("x is less than or equal to y");
+    }
+    return 0;
+}
