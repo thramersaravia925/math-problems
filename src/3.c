554b157d92c9fea5a52057da71ec9d66d460e240
thramersaravia925 <thramersaravia925@gmail.com>
2025-03-08 06:30:30 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,11 @@
+int main() {
+  float x = 3.14;
+  int y = 20;
+  double z = 12.3456789;
+
+  printf("The value of x is: %f\n", x);
+  printf("The value of y is: %d\n", y);
+  printf("The value of z is: %lf\n", z);
+
+  return 0;
+}
