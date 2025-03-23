27eb9d57479da57a59f7ffe5275f6bdfd728f16a
thramersaravia925 <thramersaravia925@gmail.com>
2025-03-23 18:10:25 +0000

    Add 16.c

---
+++ src/16.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 5;
+
+    printf("Sum: %d + %d = %d\n", x, y, x + y);
+    printf("Difference: %d - %d = %d\n", x, y, x - y);
+
+    return 0;
+}
